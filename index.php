3fcb4741a2b86dcfcc8a848a65793dfda3fd4382
michelelanotte <miki.mouse97@hotmail.it>
2016-11-05 17:30:19 +0100

    beginning of the project

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php $title = 'Home'; require('./partials/head.php'); ?>
 
-<p>CSS is <b id="not">not</b> working</p>
-<p>JS is <b id="js-not">not</b> working</p>
-
-<hr>
-
-<form action="/result.php" method="GET">
-  Oggetto da riciclare
-  <input type="text" name="object">
-  <input type="submit" value="Submit">
-</form>
+<html>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  
+  <body style="background-color: red">
+    <div class="container1">
+	  <h1>Recycle in </h1>
+	  <h1> Barletta </h1>
+	  
+	  <div class="search">
+		<form action="/result.php" method="GET">
+          <input class="search_bar" type="text" name="object" placeholder="Cerca oggetto..."><br><br>
+		  <input class="submit" type="submit" value="Cerca">
+	    </form>
+	  </div>
+	  <br>
+	  <h3>Riciclare è importante, ma non sempre facile. Recycle in Barletta ti aiuta: non sai dove mettere qualcosa? 
+		    Inserisci l’oggetto in questa pagina, noi ti diremo dove metterlo! <h3>
+	</div>
+  </body>
+</html>
 
 <?php require('./partials/tail.php'); ?>
